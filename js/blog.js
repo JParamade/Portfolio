ca7d82ad585e51380fe52b195b28ddd688e4ea27
JParamade <jaime.paramo@outlook.com>
2025-11-17 07:39:40 +0100

    Blog Updated
    + Created first blog entry.

--- js/blog.js
+++ js/blog.js
@@ -1,7 +1,10 @@
-const descriptions = document.querySelectorAll('.post-description');
-descriptions.forEach(desc => {
-    const max = parseInt(desc.dataset.maxlength);
-    if (desc.textContent.length > max) {
-        desc.textContent = desc.textContent.substring(0, max) + '...';
-    }
+/* global document */
+document.addEventListener("DOMContentLoaded", function () {
+    var descriptions = document.querySelectorAll('.post-description');
+    descriptions.forEach(function (desc) {
+        var max = parseInt(desc.dataset.maxlength);
+        if (desc.textContent.length > max) {
+            desc.textContent = desc.textContent.substring(0, max) + '...';
+        }
+    });
 });
